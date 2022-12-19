de763e45a9a0df49ec1dde1fc0e0931e8ebd9b06
Koren Cohen <80917370+Koren-Cohen@users.noreply.github.com>
2022-12-19 14:24:05 +0200

    nav bar fixed!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import PostsPage from './components/Pages/PostsPage/PostsPage';
-import Profile from './components/Pages/Profile/Profile';
-import About from './components/Pages/About/About';
-import Login from './components/Pages/Login/Login';
-import Home from './components/Pages/HomePage/Home';
-import SignUp from './components/Pages/SignUp/SignUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Sticky from 'react-sticky-el';
-import { useAuth } from './firebase';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import PostsPage from "./components/Pages/PostsPage/PostsPage";
+import Profile from "./components/Pages/Profile/Profile";
+import About from "./components/Pages/About/About";
+import Login from "./components/Pages/Login/Login";
+import Home from "./components/Pages/HomePage/Home";
+import SignUp from "./components/Pages/SignUp/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sticky from "react-sticky-el";
+import { useAuth } from "./firebase";
 
 const App = () => {
   const currentUser = useAuth(); //Uses authentication in all app pages and routers.
@@ -17,9 +17,7 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Sticky>
-          <NavBar />
-        </Sticky>
+        <NavBar />
         <Switch>
           {/* About page always appears */}
           <Route path="/about" component={About} />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import * as React from "react";
 // Font Awesome Source
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faAddressCard,
@@ -10,14 +10,14 @@ import {
   faSignInAlt,
   faInfoCircle,
   faUserPlus,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 //----------------------------------------------------------------------------//
-import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar, Container } from 'react-bootstrap';
-import './NavBar.css';
-import { Link } from 'react-router-dom';
-import { useAuth, logout } from '../../firebase';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import { useAuth, logout } from "../../firebase";
+import { useState } from "react";
 
 const Nav_Bar = () => {
   const [loading, setLoading] = useState(false);
@@ -28,78 +28,87 @@ const Nav_Bar = () => {
     try {
       await logout();
     } catch {
-      alert('Error!');
+      alert("Error!");
     }
     setLoading(false);
   }
 
   return (
-    <div className="nav_Bar_Font">
-      <Navbar bg="my-Blue" variant="dark">
-        <Container>
-          <Navbar.Brand>
-            <img alt="navLogo" src="./images/icons/favicon.ico" width="30px" height="30px" className="d-inline-block align-top" />{' '}
-            <b>
-              Present <FontAwesomeIcon icon={faArrowAltCircleRight} /> Simple
-            </b>
-          </Navbar.Brand>{' '}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" bg="light">
-              {/* when user is NOT connected => Show: */}
-              {!currentUser && (
-                <>
-                  <Nav.Link as={Link} to={'/'}>
-                    <FontAwesomeIcon icon={faHome} /> HOME
-                  </Nav.Link>
-                </>
-              )}
-              {/* when user is connected => Show: */}
-              {currentUser && (
-                <>
-                  <Nav.Link as={Link} to={'/posts_Page'}>
-                    <FontAwesomeIcon icon={faAddressCard} /> POSTS
-                  </Nav.Link>
-                  <Nav.Link as={Link} to={'/profile'}>
-                    <FontAwesomeIcon icon={faUserCircle} /> PROFILE
-                  </Nav.Link>
-                </>
-              )}
-              <Nav.Link as={Link} to={'/about'}>
-                <FontAwesomeIcon icon={faInfoCircle} /> ABOUT US
-              </Nav.Link>
-              {/* when user is NOT connected => Show: */}
-              {!currentUser && (
-                <>
-                  <Nav.Link as={Link} to={'/login'}>
-                    <FontAwesomeIcon icon={faSignInAlt} /> LOGIN
-                  </Nav.Link>{' '}
-                  <Nav.Link as={Link} to={'/signup'}>
-                    <FontAwesomeIcon icon={faUserPlus} /> SIGN UP
-                  </Nav.Link>
-                </>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-          {currentUser && (
-            <>
-              <div id="logged_User">
-                <b>Logged in user:</b> {currentUser?.email}
-              </div>
-              <div>
-                <Link to="/">
-                  <button type="button" class="btn btn-outline-light" disabled={loading || !currentUser} onClick={handleLogout}>
-                    {/* !currentUser - means when the user connected */}
-                    {/* currentUser - means when the user disconnected */}
-                    <FontAwesomeIcon icon={faSignOutAlt} />
-                  </button>
-                </Link>
-              </div>
-            </>
-          )}
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar bg="my-Blue" variant="dark" className="nav_Bar_Font" sticky="top">
+      <Container>
+        <Navbar.Brand>
+          <img
+            alt="navLogo"
+            src="./images/icons/favicon.ico"
+            width="30px"
+            height="30px"
+            className="d-inline-block align-top"
+          />{" "}
+          <b>
+            Present <FontAwesomeIcon icon={faArrowAltCircleRight} /> Simple
+          </b>
+        </Navbar.Brand>{" "}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" bg="light">
+            {/* when user is NOT connected => Show: */}
+            {!currentUser && (
+              <>
+                <Nav.Link as={Link} to={"/"}>
+                  <FontAwesomeIcon icon={faHome} /> HOME
+                </Nav.Link>
+              </>
+            )}
+            {/* when user is connected => Show: */}
+            {currentUser && (
+              <>
+                <Nav.Link as={Link} to={"/posts_Page"}>
+                  <FontAwesomeIcon icon={faAddressCard} /> POSTS
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/profile"}>
+                  <FontAwesomeIcon icon={faUserCircle} /> PROFILE
+                </Nav.Link>
+              </>
+            )}
+            <Nav.Link as={Link} to={"/about"}>
+              <FontAwesomeIcon icon={faInfoCircle} /> ABOUT US
+            </Nav.Link>
+            {/* when user is NOT connected => Show: */}
+            {!currentUser && (
+              <>
+                <Nav.Link as={Link} to={"/login"}>
+                  <FontAwesomeIcon icon={faSignInAlt} /> LOGIN
+                </Nav.Link>{" "}
+                <Nav.Link as={Link} to={"/signup"}>
+                  <FontAwesomeIcon icon={faUserPlus} /> SIGN UP
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+        {currentUser && (
+          <>
+            <div id="logged_User">
+              <b>Logged in user:</b> {currentUser?.email}
+            </div>
+            <div>
+              <Link to="/">
+                <button
+                  type="button"
+                  class="btn btn-outline-light"
+                  disabled={loading || !currentUser}
+                  onClick={handleLogout}
+                >
+                  {/* !currentUser - means when the user connected */}
+                  {/* currentUser - means when the user disconnected */}
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </button>
+              </Link>
+            </div>
+          </>
+        )}
+      </Container>
+    </Navbar>
   );
 };
 
